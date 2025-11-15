b62a6d3e398b66cb8d4678878e6a4b4192fb2274
Rafael <rafaeloliveirasouzasil@gmail.com>
2025-11-15 18:59:19 -0300

    Banco criado / Model

--- app_home/models.py
+++ app_home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    usuario = models.CharField(max_length=100,unique=True)
+    senha = models.CharField(max_length=128)
+    def __str__(self):
+        return self.usuario
--- app_home/views.py
+++ app_home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Usuario
 # Create your views here.
 
-def home(request):
-    return render(request,"./app_home/home.html") 
+def home(request): 
+    usuario = Usuario.objects.all()
+    return render(request,"./app_home/home.html",{"usuario":usuario}) 
